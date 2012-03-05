0c91d51edc520d38a5e6def68074838da1bd889c
erikkallen <erik_kallen@hotmail.com>
2012-03-05 19:23:05 +0100

    Tests for expression statement.

--- Saltarelle.Compiler.Tests/MethodCompilationTests/StatementTests.cs
+++ Saltarelle.Compiler.Tests/MethodCompilationTests/StatementTests.cs
@@ -336,5 +336,38 @@ public void M() {
 	}
 ");
 		}
+
+		[Test]
+		public void ExpressionStatementThatOnlyRequiresASingleScriptStatementWorks() {
+			AssertCorrect(
+@"public int SomeProperty { get; set; }
+public void M() {
+	int i;
+	// BEGIN
+	i = 0;
+	// END
+}",
+@"	$i = 0;
+");
+		}
+
+		[Test]
+		public void ExpressionStatementThatRequiresMultipleScriptStatementsWorks() {
+			AssertCorrect(
+@"public int P1 { get; set; }
+public int P2 { get; set; }
+public int P3 { get; set; }
+public void M() {
+	int i;
+	// BEGIN
+	i = (P1 = P2 = P3 = 1);
+	// END
+}",
+@"	this.set_P3(1);
+	this.set_P2(1);
+	this.set_P1(1);
+	$i = 1;
+");
+		}
 	}
 }
