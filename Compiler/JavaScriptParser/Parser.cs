388d4391bdf158e4a73035d42b0086db64ed5189
Erik Källén <erik_kallen@hotmail.com>
2012-07-29 13:09:42 +0200

    Started work on a JavaScript parser.

---
+++ Compiler/JavaScriptParser/Parser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Saltarelle.Compiler.JSModel.Expressions;
+using Saltarelle.Compiler.JSModel.Statements;
+
+namespace JavaScriptParser {
+	public class Parser {
+		public JsExpression ParseExpression(string expr) {
+		}
+
+		public JsStatement ParseStatement(string expr) {
+		}
+	}
+}
---
+++ Compiler/Saltarelle.Compiler.Tests/JavaScriptParserTests/JavaScriptParserTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Saltarelle.Compiler.Tests.JavaScriptParserTests {
+	[TestFixture]
+	public class JavaScriptParserTests {
+		[Test]
+		public void Identifier() {
+			var expr = JavaScriptParser.
+		}
+	}
+}
