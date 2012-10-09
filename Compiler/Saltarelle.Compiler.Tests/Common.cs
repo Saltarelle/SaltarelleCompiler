d87f79bd0bf12573682197662c074abf9bc375fa
Erik Källén <erik_kallen@hotmail.com>
2012-10-09 23:52:06 +0200

    Fixed test failures caused by the debug/release naming change.

--- Compiler/Saltarelle.Compiler.Tests/Common.cs
+++ Compiler/Saltarelle.Compiler.Tests/Common.cs
@@ -11,16 +11,16 @@ namespace Saltarelle.Compiler.Tests {
 		public static readonly string MscorlibPath = Path.GetFullPath(@"..\..\..\Runtime\bin\mscorlib.dll");
 		public static readonly string LinqPath = Path.GetFullPath(@"..\..\..\Runtime\bin\Script.Linq.dll");
 
-        private static readonly Lazy<IAssemblyReference> _mscorlibLazy = new Lazy<IAssemblyReference>(() => new CecilLoader() { IncludeInternalMembers = true }.LoadAssemblyFile(MscorlibPath));
-        internal static IAssemblyReference Mscorlib { get { return _mscorlibLazy.Value; } }
+		private static readonly Lazy<IAssemblyReference> _mscorlibLazy = new Lazy<IAssemblyReference>(() => new CecilLoader() { IncludeInternalMembers = true }.LoadAssemblyFile(MscorlibPath));
+		internal static IAssemblyReference Mscorlib { get { return _mscorlibLazy.Value; } }
 
-        private static readonly Lazy<IAssemblyReference> _linqLazy = new Lazy<IAssemblyReference>(() => new CecilLoader() { IncludeInternalMembers = true }.LoadAssemblyFile(LinqPath));
-        internal static IAssemblyReference Linq { get { return _linqLazy.Value; } }
+		private static readonly Lazy<IAssemblyReference> _linqLazy = new Lazy<IAssemblyReference>(() => new CecilLoader() { IncludeInternalMembers = true }.LoadAssemblyFile(LinqPath));
+		internal static IAssemblyReference Linq { get { return _linqLazy.Value; } }
 
-        private static readonly Lazy<string> _mscorlibScriptLazy = new Lazy<string>(() => File.ReadAllText(@"..\..\..\Runtime\bin\Script\mscorlib.debug.js"));
+		private static readonly Lazy<string> _mscorlibScriptLazy = new Lazy<string>(() => File.ReadAllText(@"..\..\..\Runtime\bin\Script\mscorlib.js"));
 		internal static string MscorlibScript { get { return _mscorlibScriptLazy.Value; } }
 
-        private static readonly Lazy<string> _linqScriptLazy = new Lazy<string>(() => File.ReadAllText(@"..\..\..\Runtime\bin\Script\linq.js"));
+		private static readonly Lazy<string> _linqScriptLazy = new Lazy<string>(() => File.ReadAllText(@"..\..\..\Runtime\bin\Script\linq.js"));
 		internal static string LinqScript { get { return _linqScriptLazy.Value; } }
 
 		public static Mock<ITypeDefinition> CreateTypeMock(string fullName) {
