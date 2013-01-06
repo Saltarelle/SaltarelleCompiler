9b404517c31def24219f62d7def28d7b8c840d15
Erik Källén <erik_kallen@hotmail.com>
2013-01-06 16:24:48 +0100

    Paper.js assembly info

---
+++ Runtime/src/Libraries/PaperJS/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿// AssemblyInfo.cs
+// Script#/Libraries/Web
+// This source code is subject to terms and conditions of the Apache License, Version 2.0.
+//
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Script.Web")]
+[assembly: AssemblyDescription("Saltarelle Paper.js implementation")]
+[assembly: ScriptAssembly("")]
