d7989718af83b3a18c836507618b7b217aceab74
Erik Källén <erik_kallen@hotmail.com>
2013-02-10 18:18:32 +0100

    Project dependency fix

--- Compiler/SCTask/SCTask.cs
+++ Compiler/SCTask/SCTask.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
-using Saltarelle.Compiler.Driver;
 
 namespace Saltarelle.Compiler.SCTask {
 	public class SCTask : Task {
