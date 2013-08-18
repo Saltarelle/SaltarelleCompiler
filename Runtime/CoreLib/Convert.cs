b6042b82a76954c0f16df4efe66150c48492e312
AstrorEnales <marcel@exdream.com>
2013-08-18 21:10:10 +0200

    Added basic Convert class

---
+++ Runtime/CoreLib/Convert.cs
@@ -0,0 +1,106 @@
+// Convert.cs
+// Script#/Libraries/CoreLib
+// This source code is subject to terms and conditions of the Apache License, Version 2.0.
+//
+
+using System.Runtime.CompilerServices;
+
+namespace System {
+	[Imported(ObeysTypeSystem = true)]
+	[ScriptNamespace("ss")]
+	public static class Convert {
+		public static float ToSingle(bool value) {
+			return 0;
+		}
+
+		// TODO: always throws InvalidCastException
+		//public static float ToSingle(char value) {
+		//	return 0;
+		//}
+
+		// TODO: always throws InvalidCastException
+		//public static float ToSingle(DateTime value) {
+		//	return 0;
+		//}
+
+		public static float ToSingle(byte value) {
+			return 0;
+		}
+
+		public static float ToSingle(decimal value) {
+			return 0;
+		}
+
+		public static float ToSingle(double value) {
+			return 0;
+		}
+
+		public static float ToSingle(short value) {
+			return 0;
+		}
+
+		public static float ToSingle(int value) {
+			return 0;
+		}
+
+		public static float ToSingle(long value) {
+			return 0;
+		}
+
+		public static float ToSingle(object value) {
+			return 0;
+		}
+
+		public static float ToSingle(sbyte value) {
+			return 0;
+		}
+
+		public static float ToSingle(float value) {
+			return 0;
+		}
+
+		public static float ToSingle(string value) {
+			return 0;
+		}
+
+		public static float ToSingle(ushort value) {
+			return 0;
+		}
+
+		public static float ToSingle(uint value) {
+			return 0;
+		}
+
+		public static float ToSingle(ulong value) {
+			return 0;
+		}
+
+		public static float ToSingle(object value, IFormatProvider provider) {
+			return 0;
+		}
+
+		public static float ToSingle(string value, IFormatProvider provider) {
+			return 0;
+		}
+
+		public static string ToString(string value) {
+			return "";
+		}
+
+		public static int ToInt32(string value) {
+			return 0;
+		}
+
+		public static double ToDouble(string value, IFormatProvider provider) {
+			return 0;
+		}
+
+		public static bool ToBoolean(string value) {
+			return false;
+		}
+
+		public static char ToChar(string value) {
+			return default(char);
+		}
+	}
+}
