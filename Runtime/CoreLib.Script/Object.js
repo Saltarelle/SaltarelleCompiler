98389153bf84704831285290296dff9cab10db47
Erik Källén <erik_kallen@hotmail.com>
2013-09-05 00:04:23 +0200

    Use Object.keys fallback from MDN (#246)

--- Runtime/CoreLib.Script/Object.js
+++ Runtime/CoreLib.Script/Object.js
@@ -13,26 +13,38 @@ ss.keyExists = function#? DEBUG ss$keyExists##(d, key) {
 };
 
 if (!Object.keys) {
-	Object.keys = function#? DEBUG Object$keys##(d) {
-		var keys = [];
-		for (var n in d) {
-			if (d.hasOwnProperty(n))
-				keys.push(n);
-		}
-		return keys;
-	};
+	Object.keys = (function() {
+		'use strict';
+		var hasOwnProperty = Object.prototype.hasOwnProperty,
+			hasDontEnumBug = !({toString: null}).propertyIsEnumerable('toString'),
+			dontEnums = ['toString','toLocaleString','valueOf','hasOwnProperty','isPrototypeOf','propertyIsEnumerable','constructor'],
+			dontEnumsLength = dontEnums.length;
 
-	ss.getKeyCount = function#? DEBUG ss$getKeyCount##(d) {
-		var count = 0;
-		for (var n in d) {
-			if (d.hasOwnProperty(n))
-				count++;
-		}
-		return count;
-	};
+		return function (obj) {
+			if (typeof obj !== 'object' && (typeof obj !== 'function' || obj === null)) {
+				throw new TypeError('Object.keys called on non-object');
+			}
+
+			var result = [], prop, i;
+
+			for (prop in obj) {
+				if (hasOwnProperty.call(obj, prop)) {
+					result.push(prop);
+				}
+			}
+
+			if (hasDontEnumBug) {
+				for (i = 0; i < dontEnumsLength; i++) {
+					if (hasOwnProperty.call(obj, dontEnums[i])) {
+						result.push(dontEnums[i]);
+					}
+				}
+			}
+			return result;
+		};
+	}());
 }
-else {
-	ss.getKeyCount = function#? DEBUG ss$getKeyCount2##(d) {
-		return Object.keys(d).length;
-	};
-}
+
+ss.getKeyCount = function#? DEBUG ss$getKeyCount##(d) {
+	return Object.keys(d).length;
+};
