3c673c5454b14905d2b18ceda6ac2a962e81f69c
Erik Källén <erik_kallen@hotmail.com>
2014-10-01 23:14:41 +0200

    Added new attributes in order to enable binary compatibility of (non-plugin) libs with the Roslyn version

---
+++ Runtime/CoreLib/InternalScriptMetadata.cs
@@ -0,0 +1,39 @@
+using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices.Internal {
+	/// <summary>
+	/// This attribute supports the infrastructure and should not be applied manually.
+	/// </summary>
+	[NonScriptable]
+	[EditorBrowsable(EditorBrowsableState.Never)]
+	[Obsolete("This attribute supports the infrastructure and should not be applied manually")]
+	public sealed class ScriptSemanticsAttribute : Attribute {
+		public object[] Data { get; private set; }
+		public ScriptSemanticsAttribute(object[] data) {
+			Data = data;
+		}
+	}
+
+	/// <summary>
+	/// This attribute supports the infrastructure and should not be applied manually.
+	/// </summary>
+	[NonScriptable]
+	[EditorBrowsable(EditorBrowsableState.Never)]
+	[Obsolete("This attribute supports the infrastructure and should not be applied manually")]
+	public sealed class UsedMemberNamesAttribute : Attribute {
+		public string[] Names { get; private set; }
+		public UsedMemberNamesAttribute(string[] names) {
+			Names = names;
+		}
+	}
+
+	[NonScriptable]
+	[EditorBrowsable(EditorBrowsableState.Never)]
+	[Obsolete("This attribute supports the infrastructure and should not be applied manually")]
+	public sealed class ScriptSerializableAttribute : Attribute {
+		public string TypeCheckCode { get; private set; }
+		public ScriptSerializableAttribute(string typeCheckCode) {
+			TypeCheckCode = typeCheckCode;
+		}
+	}
+}
