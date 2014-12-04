d0c07337b315ac0e4bc6c688dfc08d242ddd1ee8
Erik Källén <erik_kallen@hotmail.com>
2014-12-04 22:31:58 +0100

    Added a AddSourceMapDirective msbuild property that can be used to automatically insert a //#sourceMappingUrl directive into the generated script

---
+++ Compiler/SCTask/AppendBlankLineToFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Saltarelle.Compiler.SCTask {
+	public class AppendBlankLineToFile : Task {
+		public ITaskItem File { get; set; }
+
+		public override bool Execute() {
+			System.IO.File.AppendAllText(File.ItemSpec, Environment.NewLine);
+			return true;
+		}
+	}
+}
