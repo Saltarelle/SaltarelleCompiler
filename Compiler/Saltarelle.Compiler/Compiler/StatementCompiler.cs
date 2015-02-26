ac8b3b26f4b9ce1d178b910bcae9be6468a0860e
Jon Hall <jonathan.hall@webappuk.com>
2015-02-26 14:24:11 +0000

    Add the ability to switch on chars.

--- Compiler/Saltarelle.Compiler/Compiler/StatementCompiler.cs
+++ Compiler/Saltarelle.Compiler/Compiler/StatementCompiler.cs
@@ -1046,7 +1046,11 @@ namespace Saltarelle.Compiler.Compiler {
 						}
 						else if (value is string) {
 							values.Add(JsExpression.String((string)value));
-						}
+                        }
+                        else if (value is char)
+                        {
+                            values.Add(JsExpression.String(value.ToString()));
+                        }
 						else {
 							if (value is long) {
 								if (Math.Abs((long)value) > (1L << 53)) {
