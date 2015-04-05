7497d5d56c7ea6c39bf92fcd999747f8459b4449
Erik Källén <erik_kallen@hotmail.com>
2015-04-05 17:09:40 +0200

    WS

--- Runtime/CoreLib.Script/Contract.js
+++ Runtime/CoreLib.Script/Contract.js
@@ -8,95 +8,95 @@ ss.Contract = ss_Contract;
 ss.initClass(ss_Contract, ss, {});
 
 ss_Contract.reportFailure = function#? DEBUG Contract$reportFailure##(failureKind, userMessage, condition, innerException, TException) {
-    var conditionText = condition.toString();
-    conditionText = conditionText.substring(conditionText.indexOf("return") + 7);
-    conditionText = conditionText.substr(0, conditionText.lastIndexOf(";"));
+	var conditionText = condition.toString();
+	conditionText = conditionText.substring(conditionText.indexOf("return") + 7);
+	conditionText = conditionText.substr(0, conditionText.lastIndexOf(";"));
 
-    var failureMessage = (conditionText) ? "Contract '" + conditionText + "' failed" : "Contract failed";
-    var displayMessage = (userMessage) ? failureMessage + ": " + userMessage : failureMessage;
+	var failureMessage = (conditionText) ? "Contract '" + conditionText + "' failed" : "Contract failed";
+	var displayMessage = (userMessage) ? failureMessage + ": " + userMessage : failureMessage;
 
-    if (TException) {
-        throw new TException(conditionText, userMessage);
-    }
-    else {
-        throw new ss.ContractException(failureKind, displayMessage, userMessage, conditionText, innerException);   
-    }
+	if (TException) {
+		throw new TException(conditionText, userMessage);
+	}
+	else {
+		throw new ss.ContractException(failureKind, displayMessage, userMessage, conditionText, innerException);   
+	}
 };
 
 ss_Contract.assert = function#? DEBUG Contract$assert##(failureKind, condition, message) {
-    if (!condition()) {
-        ss.Contract.reportFailure(failureKind, message, condition, null);
-    }
+	if (!condition()) {
+		ss.Contract.reportFailure(failureKind, message, condition, null);
+	}
 };
 
 ss_Contract.requires = function#? DEBUG Contract$requires##(TException, condition, message) {
-    if (!condition()) {
-        ss.Contract.reportFailure(0, message, condition, null, TException);
-    }
+	if (!condition()) {
+		ss.Contract.reportFailure(0, message, condition, null, TException);
+	}
 };
 
 ss_Contract.forAll = function#? DEBUG Contract$forAll##(fromInclusive, toExclusive, predicate) {
-    if (!predicate) {
-        throw new ss.ArgumentNullException("predicate");
-    }
-    for (; fromInclusive < toExclusive; fromInclusive++) {
-        if (!predicate(fromInclusive)) {
-            return false;
-        }
-    }
-    return true;
+	if (!predicate) {
+		throw new ss.ArgumentNullException("predicate");
+	}
+	for (; fromInclusive < toExclusive; fromInclusive++) {
+		if (!predicate(fromInclusive)) {
+			return false;
+		}
+	}
+	return true;
 };
 
 ss_Contract.forAll$1 = function#? DEBUG Contract$forAll$1##(collection, predicate) {
-    if (!collection) {
-        throw new ss.ArgumentNullException("collection");
-    }
-    if (!predicate) {
-        throw new ss.ArgumentNullException("predicate");
-    }
-    var enumerator = ss.getEnumerator(collection);
-    try {
-        while (enumerator.moveNext()) {
-            if (!predicate(enumerator.current())) {
-                return false;
-            }
-        }
-        return true;
-    } finally {
-        enumerator.dispose();
-    }
+	if (!collection) {
+		throw new ss.ArgumentNullException("collection");
+	}
+	if (!predicate) {
+		throw new ss.ArgumentNullException("predicate");
+	}
+	var enumerator = ss.getEnumerator(collection);
+	try {
+		while (enumerator.moveNext()) {
+			if (!predicate(enumerator.current())) {
+				return false;
+			}
+		}
+		return true;
+	} finally {
+		enumerator.dispose();
+	}
 };
 
 ss_Contract.exists = function#? DEBUG Contract$exists##(fromInclusive, toExclusive, predicate) {
-    if (!predicate) {
-        throw new ss.ArgumentNullException("predicate");
-    }
-    for (; fromInclusive < toExclusive; fromInclusive++) {
-        if (predicate(fromInclusive)) {
-            return true;
-        }
-    }
-    return false;
+	if (!predicate) {
+		throw new ss.ArgumentNullException("predicate");
+	}
+	for (; fromInclusive < toExclusive; fromInclusive++) {
+		if (predicate(fromInclusive)) {
+			return true;
+		}
+	}
+	return false;
 };
 
 ss_Contract.exists$1 = function#? DEBUG Contract$exists$1##(collection, predicate) {
-    if (!collection) {
-        throw new ss.ArgumentNullException("collection");
-    }
-    if (!predicate) {
-        throw new ss.ArgumentNullException("predicate");
-    }
-    var enumerator = ss.getEnumerator(collection);
-    try {
-        while (enumerator.moveNext()) {
-            if (predicate(enumerator.current())) {
-                return true;
-            }
-        }
-        return false;
-    } finally {
-        enumerator.dispose();
-    }
+	if (!collection) {
+		throw new ss.ArgumentNullException("collection");
+	}
+	if (!predicate) {
+		throw new ss.ArgumentNullException("predicate");
+	}
+	var enumerator = ss.getEnumerator(collection);
+	try {
+		while (enumerator.moveNext()) {
+			if (predicate(enumerator.current())) {
+				return true;
+			}
+		}
+		return false;
+	} finally {
+		enumerator.dispose();
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////
